2f17f33df19ebf1c5c74058190a3a33e337760cf
kittyismylife <chss0520@sookmyung.ac.kr>
2024-07-19 11:28:27 +0900

    [Add] 메뉴바 작동 확인 페이지 (기본코드)

---
+++ dream/src/pages/Bakery.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+function Bakery() {
+  return <div>베이커리(웰니스빵)가 보일 화면입니다.</div>;
+}
+
+export default Bakery;
---
+++ dream/src/pages/Diary.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+function Diary() {
+  return <div>빵 레시피가 보일 화면입니다.</div>;
+}
+
+export default Diary;
