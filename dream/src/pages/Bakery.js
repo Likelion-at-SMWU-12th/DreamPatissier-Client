54bc49b0b0e57d3ed02eff5258aca736afcb1042
kangjuen <00112233pq@gmail.com>
2024-07-19 15:56:20 +0900

    [Add] Bakery.js 관련추가
    footer.jsx, Product.jsx, logoicon.svg, bbangSilde(배너이미지).png, pro-Oatmeal.png(제품이미지), 추가 앞으로는 커밋 자주할게요 .. ^^

--- dream/src/pages/Bakery.js
+++ dream/src/pages/Bakery.js
@@ -1,9 +1,27 @@
+import React from "react";
+import Footer from "../components/Footer";
+import bbangSlide from "../assets/bbangSlide.png";
+import Product from "../components/Product";
+
 //
 // 웰니스빵
 //
 
 function Bakery() {
-  return <div>베이커리(웰니스빵)가 보일 화면입니다.</div>;
+  return (
+    <div>
+      <div>
+        <img src={bbangSlide} />
+      </div>
+      <div>검색바</div>
+      <div>카테고리</div>
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Footer />
+    </div>
+  );
 }
 
 export default Bakery;
