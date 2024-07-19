0fb8f77c40837ba67803ffc346d79ef54a5537e6
kittyismylife <chss0520@sookmyung.ac.kr>
2024-07-19 11:45:40 +0900

    [Add] Users 페이지 라우터 추가

--- dream/src/App.js
+++ dream/src/App.js
@@ -3,6 +3,7 @@ import Menubar from "./pages/Menubar";
 import Bakery from "./pages/Bakery";
 import Recipes from "./pages/Recipes";
 import Diary from "./pages/Diary";
+import Users from "./pages/Users";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/Bakery" element={<Bakery />} />
         <Route path="/Recipes" element={<Recipes />} />
         <Route path="/Diary" element={<Diary />} />
+        <Route path="/Users" element={<Users />} />
       </Routes>
     </>
   );
