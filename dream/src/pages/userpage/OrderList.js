c1deeba76112a7c3684c3132e6074a46a7b0bda9
kittyismylife <chss0520@sookmyung.ac.kr>
2024-07-19 15:38:49 +0900

    [Add] 리뷰 작성 여부에 따른 버튼 구성 변경

--- dream/src/pages/userpage/OrderList.js
+++ dream/src/pages/userpage/OrderList.js
@@ -9,7 +9,7 @@ const OrderList = () => {
       date: "2024-07-19",
       image: profile,
       name: "test1",
-      tags: ["태그1", "태그2"],
+      tags: ["#프로틴", "#저당"],
       price: 10000,
       reviewed: false,
     },
@@ -18,6 +18,24 @@ const OrderList = () => {
       date: "2024-07-05",
       image: profile,
       name: "test2",
+      tags: ["태그3", "태그4"],
+      price: 15000,
+      reviewed: true,
+    },
+    {
+      id: 3,
+      date: "2024-07-05",
+      image: profile,
+      name: "test3",
+      tags: ["태그3", "태그4"],
+      price: 15000,
+      reviewed: false,
+    },
+    {
+      id: 4,
+      date: "2024-07-05",
+      image: profile,
+      name: "test4",
       tags: ["태그3", "태그4"],
       price: 15000,
       reviewed: true,
@@ -34,6 +52,7 @@ const OrderList = () => {
 
   return (
     <div className="order-list">
+      <div className="orderlist-title">주문목록</div>
       {products.map((product) => (
         <React.Fragment key={product.id}>
           <div className="product-card">
@@ -46,21 +65,36 @@ const OrderList = () => {
               />
               <div className="product-info">
                 <h3 className="product-name">{product.name}</h3>
-                <p className="product-tags">태그: {product.tags.join(", ")}</p>
-                <p className="product-price">
-                  가격: {product.price.toLocaleString()}원
-                </p>
+                <div className="product-tags">{product.tags.join("\t")}</div>
+                <div className="product-price">
+                  {product.price.toLocaleString()}원
+                </div>
               </div>
             </div>
 
             <div className="product-actions">
-              {product.reviewed ? (
-                <button onClick={() => handleAddToCartClick(product.id)}>
+              {!product.reviewed && (
+                <>
+                  <button
+                    className="write-review"
+                    onClick={() => handleReviewClick(product.id)}
+                  >
+                    리뷰쓰기
+                  </button>
+                  <button
+                    className="add"
+                    onClick={() => handleAddToCartClick(product.id)}
+                  >
+                    같은 빵 담기
+                  </button>
+                </>
+              )}
+              {product.reviewed && (
+                <button
+                  className="add"
+                  onClick={() => handleAddToCartClick(product.id)}
+                >
                   같은 빵 담기
-                </button>
-              ) : (
-                <button onClick={() => handleReviewClick(product.id)}>
-                  리뷰쓰기
                 </button>
               )}
             </div>
