1f49d13fbc1e02b3de917031da0306b38a80f089
kangjuen <00112233pq@gmail.com>
2024-07-25 02:21:29 +0900

    [Add] 회원가입 성공 페이지 UI 완성

--- dream/src/components/YellowBtn.jsx
+++ dream/src/components/YellowBtn.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import styled from "styled-components";
 
-const YellowBtn = ({ txt, onBtnClick, fontSize, type, width }) => {
+const YellowBtn = ({
+  txt,
+  onBtnClick,
+  fontSize,
+  type,
+  width,
+  borderRadius,
+  color,
+  fontWeight,
+  backgroundColor,
+  border,
+}) => {
   return (
     <ButtonBox
       onClick={onBtnClick}
       fontSize={fontSize}
       type={type}
       width={width}
+      borderRadius={borderRadius}
+      color={color}
+      fontWeight={fontWeight}
+      backgroundColor={backgroundColor}
+      border={border}
     >
       {txt || "버튼"}
     </ButtonBox>
@@ -17,13 +33,13 @@ const YellowBtn = ({ txt, onBtnClick, fontSize, type, width }) => {
 export default YellowBtn;
 
 const ButtonBox = styled.button`
-  background-color: #ffbd30;
+  background-color: ${(props) => props.backgroundColor || "#ffbd30"};
   width: ${(props) => props.width || "300px"};
   height: 40px;
-  border-radius: 5px;
-  border: none;
-  color: #471d06;
-  font-weight: 700;
+  border-radius: ${(props) => props.borderRadius || "5px"};
+  border: ${(props) => props.border || "none"};
+  color: ${(props) => props.color || "#471d06"};
+  font-weight: ${(props) => props.fontWeight || "700"};
   box-shadow: 0px 2px 4px 0px #00000040;
   cursor: pointer;
   font-size: ${(props) => props.fontSize || "15px"};
