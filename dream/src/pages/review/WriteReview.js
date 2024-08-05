d921ede3e4ac0791d27f4402f12ee00cd9890d62
kangjuen <00112233pq@gmail.com>
2024-08-05 16:21:28 +0900

    [Style] writereview 스타일, console.log 삭제, alret 문구 수정

--- dream/src/pages/review/WriteReview.js
+++ dream/src/pages/review/WriteReview.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
-import "../../styles/WriteReview.css";
+import styled from "styled-components";
 
 const WriteReview = () => {
   const location = useLocation();
@@ -15,7 +15,7 @@ const WriteReview = () => {
       setItem(location.state.item);
     } else {
       console.error("No product or item data available");
-      navigate("/");
+      navigate("/bakery");
     }
   }, [location.state, navigate]);
 
@@ -24,7 +24,7 @@ const WriteReview = () => {
 
   const handleSubmit = () => {
     if (!selectedButton || !reviewText) {
-      alert("Please select a rating and write a review.");
+      alert("모든 양식을 채워주세요.");
       return;
     }
 
@@ -37,10 +37,8 @@ const WriteReview = () => {
     const reviewData = {
       content: reviewText,
       satisfaction: selectedButton === "like" ? "S" : "D",
-      order_item_id: item.id, // OrderItem ID를 전달
+      order_item_id: item.id,
     };
-
-    console.log("Review data:", reviewData);
 
     const token = localStorage.getItem("token");
 
@@ -51,9 +49,7 @@ const WriteReview = () => {
         },
       })
       .then((response) => {
-        console.log("Review submitted:", response.data);
-
-        alert("리뷰가 제출되었습니다!");
+        alert("리뷰가 작성되었습니다.");
         navigate("/users/orders");
       })
       .catch((error) => {
@@ -70,56 +66,191 @@ const WriteReview = () => {
       : product.tags;
 
   return (
-    <div className="write-review-container">
-      <div className="write-orderlist-title">리뷰쓰기</div>
-      <div className="write-product-info">
-        <div className="write-product-show">
-          <img
-            src={product.img_src}
-            alt={product.name}
-            className="write-product-image"
-          />
-          <div className="write-product-info">
-            <h3 className="write-product-name">{product.name}</h3>
-            <div className="write-product-tags">{productTags.join(" ")}</div>
-          </div>
-        </div>
-      </div>
-      <hr className="write-hr1" />
-      <br />
-      <div className="write-orderlist-like">구매하신 빵은 만족하셨나요?</div>
-      <div className="write-product-actions2">
-        <button
-          className={`write-show-button ${
-            selectedButton === "dislike" ? "selected" : ""
-          }`}
+    <WriteReviewContainer>
+      <OrderListTitle>리뷰쓰기</OrderListTitle>
+      <ProductInfo>
+        <ProductShow>
+          <ProductImage src={product.img_src} alt={product.name} />
+          <ProductDetails>
+            <ProductName>{product.name}</ProductName>
+            <ProductTags>{productTags.join(" ")}</ProductTags>
+          </ProductDetails>
+        </ProductShow>
+      </ProductInfo>
+      <StyledHr />
+      <OrderListLike>구매하신 빵은 만족하셨나요?</OrderListLike>
+      <ProductActions>
+        <ShowButton
+          className={selectedButton === "dislike" ? "selected" : ""}
           onClick={() => setSelectedButton("dislike")}
         >
           별로예요 👎🏻
-        </button>
-        <button
-          className={`write-show-button ${
-            selectedButton === "like" ? "selected" : ""
-          }`}
+        </ShowButton>
+        <ShowButton
+          className={selectedButton === "like" ? "selected" : ""}
           onClick={() => setSelectedButton("like")}
         >
           만족해요 👍🏻
-        </button>
-      </div>
-      <div className="write-orderlist-like">자세한 리뷰를 작성해 주세요</div>
-      <textarea
-        className="write-text_review"
+        </ShowButton>
+      </ProductActions>
+      <OrderListLike>자세한 리뷰를 작성해 주세요</OrderListLike>
+      <ReviewTextarea
         rows="9"
         value={reviewText}
         onChange={(e) => setReviewText(e.target.value)}
       />
-      <div className="write-submit-button-container">
-        <button className="write-submit-review" onClick={handleSubmit}>
-          제출
-        </button>
-      </div>
-    </div>
+      <SubmitButtonContainer>
+        <SubmitButton onClick={handleSubmit}>리뷰 등록하기</SubmitButton>
+      </SubmitButtonContainer>
+    </WriteReviewContainer>
   );
 };
 
 export default WriteReview;
+
+// Styled Components 정의
+
+const WriteReviewContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0px;
+  margin: 0px;
+`;
+
+const OrderListTitle = styled.div`
+  width: 80%;
+  font-weight: 700;
+  line-height: 22px;
+  font-size: 18px;
+  margin: 20px 20px 20px 20px;
+  letter-spacing: -0.5px;
+`;
+
+const ProductInfo = styled.div`
+  width: 90%;
+  height: auto;
+  align-items: center;
+`;
+
+const ProductShow = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 0px 25px;
+`;
+
+const ProductImage = styled.img`
+  border-radius: 10px;
+  width: 90px;
+  height: 90px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.15);
+`;
+
+const ProductDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 15px;
+  margin-bottom: px;
+  justify-content: center;
+`;
+
+const ProductName = styled.h3`
+  color: #471d06;
+  font-size: 13px;
+  font-weight: 700;
+  letter-spacing: -0.5px;
+  margin: 0 0 5px 0;
+`;
+
+const ProductTags = styled.div`
+  color: #ffb415;
+  font-weight: 700;
+  font-size: 11px;
+`;
+
+const StyledHr = styled.hr`
+  margin-top: 25px;
+  margin-bottom: 25px;
+  border: 0.5px solid #ccc;
+  width: 100%;
+  background-color: #d9d9d9;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.3);
+`;
+
+const OrderListLike = styled.div`
+  color: #471d06;
+  width: 80%;
+  font-weight: bold;
+  font-size: 16px;
+  margin: 0px 20px 15px 0;
+  font-family: "Noto Sans KR";
+`;
+
+const ProductActions = styled.div`
+  margin: 0 18px;
+  margin-bottom: 40px;
+  display: flex;
+  flex-direction: row;
+`;
+
+const ShowButton = styled.button`
+  flex: 1;
+  background-color: white;
+  border: 1px solid #471d06;
+  width: 38vw;
+  padding: 9px 0;
+  margin: 0 15px;
+  color: #471d06;
+  font-size: 14px;
+  font-weight: 700;
+  border-radius: 5px;
+  cursor: pointer;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  transition: background-color 0.3s, color 0.3s;
+
+  &.selected {
+    background-color: #ffde97;
+    color: #77300a;
+  }
+`;
+
+const ReviewTextarea = styled.textarea`
+  display: block;
+  border: 1px solid #471d06;
+  border-radius: 8px;
+  margin: 0 auto 20px auto;
+  width: 80%;
+  padding: 10px;
+  font-size: 16px;
+
+  &:focus {
+    border: 1px solid #ffb415;
+    outline: none;
+  }
+`;
+
+const SubmitButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  margin: 20px 0 40px 0;
+`;
+
+const SubmitButton = styled.button`
+  width: 85%;
+  background-color: #ffc851;
+  border: 0;
+  padding: 8px 10px;
+  margin: 0 10px;
+  color: #471d06;
+  font-size: 16px;
+  font-weight: 800;
+  border-radius: 5px;
+  cursor: pointer;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.25);
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #ffb415;
+  }
+`;
